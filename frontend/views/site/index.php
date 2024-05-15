c281567303da7dac1002331412e255ab722fbcb1
Arthur Pires de Paula Lima <arthur.lima@invisual.com.br>
2024-05-15 10:12:11 -0300

    Master-mirror: Subindo ajuste no layout de index site

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -12,8 +12,17 @@ $imageUrl = Url::to('@web/uploads/images.jpg');
 
 <style>
     .card-img-top {
-        height: 400px; /* Defina a altura desejada para as imagens */
+        height: 400px; /*Defina a altura desejada para as imagens*/
         object-fit: cover; /* Ajusta a imagem para cobrir a área designada */
+    }
+    .position-relative {
+        position: relative;
+    }
+    .img-overlay {
+        width: 70px;
+        position: absolute;
+        bottom: 10px;
+        right: 10px;
     }
 </style>
 
@@ -27,25 +36,29 @@ $imageUrl = Url::to('@web/uploads/images.jpg');
 
     <div class="body-content">
 
-        <div class="container">
-            <div class="row">
-                <?php foreach($filmesModel as $filme){ ?>
-                    <div class="col-md-4 mb-4">
-                        <div class="card">
-                            <img src="<?= Url::to('@web/' . $filme->logo) ?>" class="card-img-top" alt="<?= $filme->nome ?>">
-                            <div class="card-body">
-                                <img src="<?= Url::to('@web/' . $filme->classificacao->imagem_classificacao) ?>" class="img-fluid" width = "13px" height = "13px" alt="<?= $filme->nome ?>">
-                                <h5 class="card-title"><?= $filme->nome ?></h5>
-                                <p>Categoria: Ação</p>
-                                <p>Classificação: L</p>
-                                <p>Valor diária: R$ 10.45</p>
-                                <a href="#" class="btn btn-primary">Alugar</a>
-                            </div>
+    <div class="container">
+    <div class="row">
+        <?php foreach($filmesModel as $filme): ?>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <div class="position-relative">
+                        <img src="<?= Url::to('@web/' . $filme->logo) ?>" class="card-img-top" alt="<?= $filme->nome ?>">
+                        <div class="img-overlay d-flex align-items-end justify-content-end">
+                            <img src="<?= Url::to('@web/' . $filme->classificacao->imagem_classificacao) ?>" class="img-fluid" alt="<?= $filme->nome ?>">
                         </div>
                     </div>
-                <?php } ?>
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $filme->nome ?></h5>
+                        <p>Categoria: Ação</p>
+                        <p>Classificação: L</p>
+                        <p>Valor diária: R$ 10.45</p>
+                        <a href="#" class="btn btn-primary">Alugar</a>
+                    </div>
+                </div>
             </div>
-        </div>
+        <?php endforeach; ?>
+    </div>
+</div>
 
     </div>
 </div>
